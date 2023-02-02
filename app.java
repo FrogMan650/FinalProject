a7bc9900a16735ff95cc08a6c50d7fae6fd44eab
FrogMan650 <123999867+FrogMan650@users.noreply.github.com>
2023-02-01 19:40:01 -0500

    Add files via upload

---
+++ app.java
@@ -0,0 +1,19 @@
+package finalproject;
+
+public class app {
+
+    public static void main(String[] args) {
+        myJFrame mjf = new myJFrame();
+    }
+    
+}
+//group 23
+//mapJPanel = Map
+//JPanelC1-5 = Campus 1-5
+//JPanelWC = World Campus
+//JPanelCS = Character Select
+//JPanelSB = Scoreboard
+//JPanelSP = Score panel
+//JPanelD = Directions
+//JPanelI = Instructions
+//JPanelT = Theme select
